d341952692049a28a60acec2e2e143d1e3550f73
gleb <dgbce@tu.komus.net>
2017-04-14 14:57:31 +0300

    Переопределил папку нахождения файла синонимов в фильтрах.

---
+++ solr/contrib/komus/src/java/ru/komus/ysolr/stopwords/StopWordsFilterFactory.java
@@ -0,0 +1,20 @@
+package ru.komus.ysolr.stopwords;
+
+import de.hybris.platform.solrfacetsearch.ysolr.stopwords.HybrisStopWordsFilterFactory;
+import org.apache.solr.core.SolrResourceLoader;
+
+import java.io.File;
+import java.util.Map;
+
+public class StopWordsFilterFactory extends HybrisStopWordsFilterFactory {
+    public StopWordsFilterFactory(Map<String, String> args) {
+        super(args);
+    }
+
+    @Override
+    protected File getDataFile(SolrResourceLoader loader, String filename) {
+        String dir = loader.getInstanceDir();
+        return dir == null ? null : new File(dir + File.separator + "ydata" + File.separator + filename + this.langCode + ".txt");
+
+    }
+}
---
+++ solr/contrib/komus/src/java/ru/komus/ysolr/synonyms/SynonymFilterFactory.java
@@ -0,0 +1,20 @@
+package ru.komus.ysolr.synonyms;
+
+import de.hybris.platform.solrfacetsearch.ysolr.synonyms.HybrisSynonymFilterFactory;
+import org.apache.solr.core.SolrResourceLoader;
+
+import java.io.File;
+import java.util.Map;
+
+public class SynonymFilterFactory extends HybrisSynonymFilterFactory {
+    public SynonymFilterFactory(Map<String, String> args) {
+        super(args);
+    }
+
+    @Override
+    protected File getDataFile(SolrResourceLoader loader, String filename) {
+        String dir = loader.getInstanceDir();
+        return dir == null ? null : new File(dir + File.separator + "ydata" + File.separator + filename + this.langCode + ".txt");
+
+    }
+}
